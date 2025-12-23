a6de1789d8109dd7717c4a6f5b5284affc8de2a1
Imansa Sandil <imansasandil@gmail.com>
2025-12-23 14:05:42 +0530

    added matchBedrooms function

--- estate-application/src/utils/search.js
+++ estate-application/src/utils/search.js
@@ -4,3 +4,10 @@ export function matchesPrice(property, minPrice, maxPrice) {
   if (maxPrice != null && price > maxPrice) return false;
   return true;
 }
+
+export function matchesBedrooms(property, minBedrooms, maxBedrooms) {
+    const {bedrooms} = property;
+    if (minBedrooms != null && bedrooms < minBedrooms) return false;
+    if (maxBedrooms != null && bedroom > maxBedrooms) return false;
+    return true;
+}
