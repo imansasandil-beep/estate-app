b41eeb45fdf030c73f24a36098976db3e6c14dd0
Imansa Sandil <imansasandil@gmail.com>
2025-12-29 22:17:29 +0530

    Added property card with drag and drop and favourite button

---
+++ estate-application/src/components/PropertyCard.jsx
@@ -0,0 +1,43 @@
+function PropertyCard({ property, onView, onAddFavourite, isFavourite }) {
+  const handleDragStart = (event) => {
+    event.dataTransfer.setData('text/plain', property.id);
+    event.dataTransfer.effectAllowed = 'copy';
+  };
+
+  return (
+    <article className="property-card" draggable onDragStart={handleDragStart}>
+      <img
+        src={property.thumbnail}
+        alt={property.shortDescription}
+        className="property-image"
+      />
+      <div className="property-content">
+        <h3 className="property-title">
+          {property.type} in {property.postcodeArea}
+        </h3>
+        <p className="property-location">{property.location}</p>
+        <p className="property-price">
+          £{property.price.toLocaleString('en-GB')}
+        </p>
+        <p className="property-meta">
+          {property.bedrooms} bedrooms • {property.tenure}
+        </p>
+        <p className="property-description">{property.shortDescription}</p>
+        <div className="property-actions">
+          <button type="button" onClick={() => onView(property.id)}>
+            View details
+          </button>
+          <button
+            type="button"
+            className={isFavourite ? 'secondary' : ''}
+            onClick={() => onAddFavourite(property.id)}
+          >
+            {isFavourite ? 'In favourites' : 'Add to favourites'}
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default PropertyCard;
