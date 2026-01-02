83eabc2b83ac4009feefde25b7f0843a3f8df5f0
Imansa Sandil <imansasandil@gmail.com>
2026-01-02 23:52:11 +0530

    updated package.json files and installed github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' // (Or vue, depending on your setup)
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/estate-app/",  // <--- ADD THIS LINE! (Make sure the name matches your repo name exactly)
 })
